183f132c8db9c7cea43db8795e6abf071270ceb7
GOTY8 <k66sopo@naver.com>
2020-02-12 19:39:23 +0900

    3강까지 웹 애플리케이션 작성

--- src/main/java/com/hungrybug/webservice/web/WebRestController.java
+++ src/main/java/com/hungrybug/webservice/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.hungrybug.webservice.web;
 
+import com.hungrybug.webservice.domain.posts.PostsRepository;
+import com.hungrybug.webservice.dto.posts.PostsSaveRequestDto;
+import com.hungrybug.webservice.service.PostsService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
+    private PostsService postsService;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public Long savePosts(@RequestBody PostsSaveRequestDto dto){
+        return postsService.save(dto);
     }
 }
---
+++ src/test/java/com/hungrybug/webservice/service/PostServiceTest.java
@@ -0,0 +1,49 @@
+package com.hungrybug.webservice.service;
+
+import com.hungrybug.webservice.domain.posts.Posts;
+import com.hungrybug.webservice.domain.posts.PostsRepository;
+import com.hungrybug.webservice.dto.posts.PostsSaveRequestDto;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class PostServiceTest {
+
+    @Autowired
+    private PostsService postsService;
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach
+    public void cleanup () {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Dto데이터가_posts테이블에_저장된다 () {
+        //given
+        PostsSaveRequestDto dto = PostsSaveRequestDto.builder()
+                .author("jojoldu@gmail.com")
+                .content("테스트")
+                .title("테스트 타이틀")
+                .build();
+
+        //when
+        postsService.save(dto);
+
+        //then
+        int newPostNum = postsRepository.findAll().size() - 1;
+        Posts posts = postsRepository.findAll().get(newPostNum);
+        assertThat(posts.getAuthor()).isEqualTo(dto.getAuthor());
+        assertThat(posts.getContent()).isEqualTo(dto.getContent());
+        assertThat(posts.getTitle()).isEqualTo(dto.getTitle());
+    }
+}
