f946f29baa7f0241a0d361b009dc2885251262a2
GOTY8 <k66sopo@naver.com>
2020-02-13 15:16:04 +0900

    무중단 배포 업데이트

--- src/main/java/com/hungrybug/webservice/web/WebRestController.java
+++ src/main/java/com/hungrybug/webservice/web/WebRestController.java
@@ -4,17 +4,20 @@ import com.hungrybug.webservice.domain.posts.PostsRepository;
 import com.hungrybug.webservice.dto.posts.PostsSaveRequestDto;
 import com.hungrybug.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -24,5 +27,12 @@ public class WebRestController {
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto){
         return postsService.save(dto);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
